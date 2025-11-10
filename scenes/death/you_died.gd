e8eb73722692346cc31e30fbad7ec68d1c8be4ac
Jeff <jeffreysabol345@gmail.com>
2025-11-10 00:25:57 -0500

    Fix timer possibly not going off

--- scenes/death/you_died.gd
+++ scenes/death/you_died.gd
@@ -1,6 +1,7 @@
 extends Control
 
 func _unhandled_input(event: InputEvent) -> void:
+	await get_tree().create_timer(3).timeout
 	if event is InputEventKey and event.pressed:
 		get_tree().change_scene_to_file("res://scenes/menus/main_menu/main_menu_with_animations.tscn")
 	elif event is InputEventMouseButton and event.pressed:
